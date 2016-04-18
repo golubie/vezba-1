27bae7b6a20c0bbab5171faf62df9ca7cbc06137
Golubie <nikola.golux@gmail.com>
2016-04-18 17:14:02 +0200

    The create article form is now updated with CSS code

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   root 'welcome#home'
   get 'about', to: 'welcome#about'
+  get 'new', to: 'article#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
